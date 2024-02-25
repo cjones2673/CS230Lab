79eb65eb33365ab04712d149ca257f33eb794af5
caj00017 <caj00017@mix.wvu.edu>
2024-02-24 23:11:20 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>CS 230L</h1>
         <h2>Section - 001</h2>
-          <p>"WVU ID: 800382612</p>
+          <p>WVU ID: 800382612</p>
           <p>Hi I am Christopher Jones</p>
     </div>
   );
